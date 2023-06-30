39b9ff48bd3a1ed7edbc97c56d6f18ae90e9a6a4
ssdd33 <ssdd33>
2023-06-30 15:42:46 +0900

    대략적인 ui 작성

--- lib/ui/timer_screen.dart
+++ lib/ui/timer_screen.dart
@@ -5,6 +5,49 @@ class Timer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            TimeDisplay(),
+            Buttons(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TimeDisplay extends StatelessWidget {
+  const TimeDisplay({super.key});
+//listen time state
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Buttons extends StatelessWidget {
+  const Buttons({super.key});
+//listen timerStatus state
+
+  Widget startButton() {
+    return IconButton(onPressed: () {}, icon: const Icon(Icons.start_outlined));
+  }
+
+  Widget puasueButton() {
+    return IconButton(
+        onPressed: () {}, icon: const Icon(Icons.pause_circle_filled_outlined));
+  }
+
+  Widget ResetButton() {
+    return IconButton(
+        onPressed: () {}, icon: const Icon(Icons.refresh_outlined));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
